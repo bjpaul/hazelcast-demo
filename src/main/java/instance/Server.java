5171acdefa5e0f28752a5b458ad6354678305b76
Bijoy Paul <bijoy.paul@tothenew.com>
2016-06-18 16:25:50 +0530

    membership listner added

--- src/main/java/instance/Server.java
+++ src/main/java/instance/Server.java
@@ -2,8 +2,13 @@ package instance;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.JoinConfig;
+import com.hazelcast.core.Cluster;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.Member;
+import com.hazelcast.nio.Address;
+
+import java.util.Set;
 
 /**
  * Created by bijoy on 17/6/16.
@@ -35,7 +40,9 @@ public class Server {
         }
 
         private static HazelcastInstance createInstance() {
-            return Hazelcast.newHazelcastInstance(config);
+            hazelcastInstance = Hazelcast.newHazelcastInstance(config);
+            membersString(hazelcastInstance.getCluster());
+            return hazelcastInstance;
         }
 
         private static Config serverConfig(){
@@ -54,6 +61,25 @@ public class Server {
                     .setEnabled(true)
                     .addMember("localhost");
             return config;
+        }
+
+        private static void membersString(Cluster cluster){
+            Set<Member> members = cluster.getMembers();
+            Member localMember = cluster.getLocalMember();
+            Address address;
+
+            System.out.print("Members [" + members.size() + "] {");
+            for (Member member : members) {
+                address = member.getAddress();
+                System.out.print("\n {"+member.getStringAttribute("user")+" -> Instance "+address.getPort() % 10+"} ");
+                System.out.print("[" + address.getHost() + "]:" + address.getPort());
+                if(localMember.getAddress().equals(address)) {
+                    System.out.print(" this");
+                }
+            }
+            System.out.println("\n}");
+
+            cluster.addMembershipListener(new CustomMembershipListner());
         }
 
     }
