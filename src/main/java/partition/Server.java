f5a214fc9f04da4e44df5b8f6625580b5283e1bf
Bijoy Paul <bijoy.paul@tothenew.com>
2016-06-23 10:32:17 +0530

    refactored

--- src/main/java/partition/Server.java
+++ src/main/java/partition/Server.java
@@ -18,7 +18,7 @@ public class Server {
         Config config = new Config();
         config.getMemberAttributeConfig().setStringAttribute("user", System.getProperty("user.name"));
         hazelcastInstance = Hazelcast.newHazelcastInstance(config);
-        membersString(hazelcastInstance.getCluster());
+        hazelcastInstance.getCluster().addMembershipListener(new CustomMembershipListner());
 
         /*
         config.setProperty("hazelcast.partition.count","350");*/
@@ -34,22 +34,5 @@ public class Server {
 
     }
 
-    private static void membersString(Cluster cluster){
-        Set<Member> members = cluster.getMembers();
-        Member localMember = cluster.getLocalMember();
-        Address address;
 
-        System.out.print("Members [" + members.size() + "] {");
-        for (Member member : members) {
-            address = member.getAddress();
-            System.out.print("\n {"+member.getStringAttribute("user")+" -> Instance "+address.getPort() % 10+"} ");
-            System.out.print("[" + address.getHost() + "]:" + address.getPort());
-            if(localMember.getAddress().equals(address)) {
-                System.out.print(" this");
-            }
-        }
-        System.out.println("\n}");
-
-        cluster.addMembershipListener(new CustomMembershipListner());
-    }
 }
